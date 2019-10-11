639493a4cb81473ead8de601c630084b1ce7dbd7
Alexander Bazo <alexanderbazo@googlemail.com>
2019-10-11 16:05:55 +0200

    Fix colors in XML and improve visual appearance

--- src/de/ur/mi/bouncer/apps/AppConfiguration.java
+++ src/de/ur/mi/bouncer/apps/AppConfiguration.java
@@ -13,14 +13,14 @@ public class AppConfiguration {
     public static final double DEFAULT_BOUNCER_SCALE_FACTOR = 0.8;
     public static final String FIELD_NAME_BOUNCER = "bouncer";
     public static final String FIELD_NAME_OBSTACLE = "obstacle";
-    public static final String FIELD_NAME_COLOR = "fieldColor";
+    public static final String FIELD_NAME_COLOR = "color";
 	public static final double DEFAULT_BOUNCER_EYE_SCALE_FACTOR = 0.2;
-    public static final Color DEFAULT_BOUNCER_COLOR = new Color(220, 220, 20);
-    public static final Color DEFAULT_GRID_COLOR = new Color(30, 30, 30);
-    public static final Color DEFAULT_BLOCK_COLOR = new Color(30, 30, 30);
-    public static final Color DEFAULT_FIELD_COLOR = new Color(255, 255, 255);
-    public static final Color RED_FIELD_COLOR = new Color(220, 20, 20);
-    public static final Color GREEN_FIELD_COLOR = new Color(20, 220, 20);
-    public static final Color BLUE_FIELD_COLOR = new Color(20, 20, 220);
+    public static final Color DEFAULT_BOUNCER_COLOR = new Color(251, 208, 0);
+    public static final Color DEFAULT_GRID_COLOR = new Color(0,0,0);
+    public static final Color DEFAULT_BLOCK_COLOR = new Color(0,0,0);
+    public static final Color DEFAULT_FIELD_COLOR = new Color(250, 250, 250);
+    public static final Color RED_FIELD_COLOR = new Color(229, 37, 33);
+    public static final Color GREEN_FIELD_COLOR = new Color(67, 176, 71);
+    public static final Color BLUE_FIELD_COLOR = new Color(4, 156, 216);
 
 }
